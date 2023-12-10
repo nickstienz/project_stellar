ba1ee986a8cc84a6790d92100b9733b4fd3a873c
Nicholas Stienz <nickstienz@gmail.com>
2023-12-10 14:09:14 -0600

    Learning a bit of Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, hello_world)
-        .run();
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
 
-fn hello_world() {
-    info!("Hello World from Bevy!");
+struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, create_people)
+            .add_systems(Update, greet);
+    }
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn create_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Nick".to_string())));
+}
+
+fn greet(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
+    }
 }
